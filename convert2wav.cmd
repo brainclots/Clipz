24bd712d01d2386e440be7e1c1d05de36cbf5314
brainclots <Brian.Klotz@outlook.com>
2018-09-06 09:53:54 -0700

    Formatting fixes

--- convert2wav.cmd
+++ convert2wav.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+if "%1" == "" echo Usage: %0 ^<mp3_filename^> [^<desired_wav_filename^>] & exit /b
+
 set CMD_DIR=%USERPROFILE%\Downloads\ffmpeg-win-2.2.2
 set CONVERTERAPP=ffmpeg.exe
 set MP3="%1"
@@ -11,8 +13,6 @@ if not defined DEST_WAV set DEST_WAV=%BASENAME%.wav
 
 set DEST_DIR=%~dp2
 if not defined DEST_DIR set DEST_DIR=.
-
-if "%1" == "" echo Usage: %0 ^<mp3_filename^> [^<desired_wav_filename^>] & exit /b
 
 %CMD_DIR%\%CONVERTERAPP% -y -i %MP3% "%DEST_DIR%\%DEST_WAV%" 2> nul
 
