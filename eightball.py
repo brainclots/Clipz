1a8ac45bd643d943e98293c5c640b25207ad2d29
brainclots <Brian.Klotz@outlook.com>
2018-12-03 15:20:58 -0800

    All in python

--- eightball.py
+++ eightball.py
@@ -6,6 +6,25 @@ import datetime
 import time
 import sys
 
+da_ball = '''
+
+        _.a$$$$$a._
+      ,$$$$$$$$$$$$$.
+    ,$$$$$$$$$$$$$$$$$.
+   d$$$$$$$$$$$$$$$$$$$b
+  d$$$$$$$$~'"`~$$$$$$$$b
+ ($$$$$$$p   _   q$$$$$$$)
+ $$$$$$$$   (_)   $$$$$$$$
+ $$$$$$$$   (_)   $$$$$$$$
+ ($$$$$$$b       d$$$$$$$)
+  q$$$$$$$$a._.a$$$$$$$$p
+   q$$$$$$$$$$$$$$$$$$$p
+    `$$$$$$$$$$$$$$$$$'
+      `$$$$$$$$$$$$$'
+        `~$$$$$$$~'
+            '''
+
+
 def get_it():
     google_drive = os.getenv('GDRIVE')
     google_drive = google_drive.replace('"', '')
@@ -14,8 +33,8 @@ def get_it():
     mp3_filenames = glob.glob(target_dir + '/*.mp3')
 
     all_filenames = wav_filenames + mp3_filenames
-    all_filenames.sort(key=str.lower)
-    lucky_one = random.choice(all_filenames)
+    random.shuffle(all_filenames)
+    lucky_one = all_filenames.pop()
     word_file = lucky_one + ".txt"
 
     if os.path.isfile(word_file):
@@ -60,46 +79,52 @@ def track_it(all_filenames, lucky_one):
     totalcount = len(all_filenames)
     with open('eightball.log', 'a') as f:
         now = datetime.datetime.now().strftime('%m/%d/%Y,%H:%M:%S')
-        f.write(f'{now},{all_filenames.index(lucky_one)},{totalcount},{os.path.basename(lucky_one)}\n')
-    nap_length = random.randrange(15, 120)
-    rest_it(nap_length)
+        f.write(f'{now},{os.path.basename(lucky_one)}\n')
+    if len(sys.argv) > 1:
+        nap_length = random.randrange(15, 120)
+        rest_it(nap_length)
 
 
 def rest_it(nap_length, now=datetime.datetime.now):
-    if len(sys.argv) > 1:
-        try:
-            target = now()
-            one_second_later = datetime.timedelta(seconds=1)
-            file = 0
-            direction = 'right'
+    try:
+        target = now()
+        one_second_later = datetime.timedelta(seconds=1)
+        os.system('cls')
+        os.system('title ~~~ Shaking the Eightball ~~~')
+        os.system('color 20')
+        os.system('mode con: cols=53 lines=22')
+        timesthru = 0
+        num_indents = -1
+        for remaining in range(nap_length, 0, -1):
+            target += one_second_later
             os.system('cls')
-            os.system('title ~~~ Shaking the Eightball ~~~')
-            os.system('color 20')
-            os.system('mode con: cols=55 lines=22')
-            for remaining in range(nap_length, 0, -1):
-                target += one_second_later
-                os.system('cls')
-                print()
-                os.system(f'type eightball{file}.txt')
-                print()
-                print(f'{" ":15}{"Sleeping for "}{str(remaining)}{" seconds..."}')
-                if direction == 'right':
-                    if file <= 3:
-                        file += 1
-                    else:
-                        direction = 'left'
-                else:
-                    if file > 0:
-                        file -= 1
-                    else:
-                        direction = 'right'
-                time.sleep((target - now()).total_seconds())
-            get_it()
-        except KeyboardInterrupt:
-            os.system('cls')
-            os.system('color 07')
-            os.system('mode con: cols=120 lines=30')
-            exit()
+            if timesthru < 5:
+                timesthru += 1
+                num_indents += 1
+            elif (timesthru >= 5) and (timesthru <= 8):
+                timesthru += 1
+                num_indents -= 1
+            else:
+                timesthru = 0
+                num_indents = 0
+            print(indent(da_ball,num_indents))
+            print(f'{"Sleeping for ":>27}{str(remaining)}{" seconds..."}')
+            time.sleep((target - now()).total_seconds())
+        get_it()
+    except KeyboardInterrupt:
+        os.system('cls')
+        os.system('color 07')
+        os.system('mode con: cols=120 lines=30')
+        exit()
+
+
+def indent(a_string, num_indents=1):
+    times = 0
+    while times < num_indents:
+        a_string = a_string.replace('\n', '\n     ')
+        times += 1
+    return a_string
+
 
 if __name__ == '__main__':
     get_it()
